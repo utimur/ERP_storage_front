b03d053071849e3e57dbd8a037e1a7ea6b1f0d7d
utimur <akva_ten@mail.ru>
2020-12-10 12:01:43 +0300

    add icon btn

--- src/components/main/Board.jsx
+++ src/components/main/Board.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'
+import React, {useEffect, useRef} from 'react'
 import Card from '@material-ui/core/Card'
-import { Button, CardActionArea, CardContent, Typography } from '@material-ui/core'
+import {Button, CardActionArea, CardContent, Icon, IconButton, Typography} from '@material-ui/core'
 import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
 import Order from './Order'
@@ -8,59 +8,69 @@ import canban from '../../store/canban'
 import { observer } from 'mobx-react-lite'
 import app from '../../store/app'
 import { statuses } from '../../utils/consts'
+import '../../App.css'
 
 const statusOrder = {
-  [statuses.CREATED]: 0,
-  [statuses.FORMALIZING]: 1,
-  [statuses.COLLECTING] : 2,
-  [statuses.DELIVERING]: 3,
-  [statuses.DELIVERED]: 4,
+    [statuses.CREATED]: 0,
+    [statuses.FORMALIZING]: 1,
+    [statuses.COLLECTING] : 2,
+    [statuses.DELIVERING]: 3,
+    [statuses.DELIVERED]: 4,
 }
 
 const Board = observer(({ board }) => {
-  const orders = canban.getOrdersByStatus(board.status)
+    const orders = canban.getOrdersByStatus(board.status)
 
-  const isMoved = (orderStatus, boardStatus) => {
-    const difference = statusOrder[canban.currentOrder.status] - statusOrder[board.status]
-    return difference === 1 || difference === -1;
+    const isMoved = (orderStatus, boardStatus) => {
+        const difference = statusOrder[canban.currentOrder.status] - statusOrder[board.status]
+        return difference === 1 || difference === -1;
 
-  }
-  const onDropEvent = (e) => {
-    e.preventDefault();
-    if (canban.currentOrder.status !== statuses.DELIVERED && isMoved(canban.currentOrder.status, board.status)) {
-        canban.currentOrder.status = board.status;
-        canban.currentOrder.order = orders.length;
-        canban.setCurrentOrder(null)
     }
-  }
+    const onDropEvent = (e) => {
+        e.preventDefault();
+        if (canban.currentOrder.status !== statuses.DELIVERED && isMoved(canban.currentOrder.status, board.status)) {
+            canban.currentOrder.status = board.status;
+            canban.currentOrder.order = orders.length;
+            canban.setCurrentOrder(null)
+        }
+    }
 
-  const onDragOverEvent = (e) => {
-    if (isMoved(canban.currentOrder.status, board.status))
-      e.preventDefault()
-  }
+    const onDragOverEvent = (e) => {
+        if (isMoved(canban.currentOrder.status, board.status))
+            e.preventDefault()
+    }
 
-  useEffect(() => {
+    useEffect(() => {
 
-  })
-  return (
-    <Card
-      style={{ height: '100%' }} onDragOver={onDragOverEvent}
-      onDrop={onDropEvent}
-    >
-      <CardContent style={{ width: 200, height: '90%' }}>
-        <Grid container justify='center'>
-          <Typography variant='h6'>
-            {board.title}
-          </Typography>
-        </Grid>
-        {canban.getOrdersByStatus(board.status).map(card =>
-          <Order key={card.id} card={card} />
-        )}
-      </CardContent>
-      {board.status === statuses.CREATED &&
-        <Button onClick={() => app.showOrderDialog()}>Новый</Button>}
-    </Card>
-  )
+    })
+    return (
+        <Card
+            style={{ height: '100%' }}
+            onDragOver={onDragOverEvent}
+            onDrop={onDropEvent}
+        >
+            <CardContent style={{ width: 200, height: '90%' }}>
+                <Grid container justify='center'>
+                    <Typography variant='h6'>
+                        {board.title}
+                    </Typography>
+                </Grid>
+                {canban.getOrdersByStatus(board.status).map(card =>
+                    <Order key={card.id} card={card} />
+                )}
+            </CardContent>
+            {board.status === statuses.CREATED &&
+                <Grid container justify="center">
+                    <Icon
+                        fontSize="large"
+                        style={{cursor: "pointer"}}
+                        onClick={() => app.showOrderDialog()}>
+                        add_circle_outline
+                    </Icon>
+                </Grid>
+            }
+        </Card>
+    )
 })
 
 export default Board
